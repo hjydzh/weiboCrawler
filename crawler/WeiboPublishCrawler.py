d286da8a20f801294c41c09c3b39bfb117dd7d65
hjydzh <452034476@qq.com>
2016-01-05 23:30:05 +0800

    添加cookie登录

--- crawler/WeiboPublishCrawler.py
+++ crawler/WeiboPublishCrawler.py
@@ -15,9 +15,11 @@ class WeiboPublishCrawler:
         self.browser = browser
 
     def publish(self):
-        LoginController.login(self.browser, '2823128008@qq.com', 'a13870093884')
+        #LoginController.log_in(self.browser, '2823128008@qq.com', 'a13870093884')
+        LoginController.login_by_cookie(self.browser)
         #WeiboPageCommon.scroll(self.browser)
         time.sleep(4)
+        self.browser.set_page_load_timeout(120)
         weibo_list =  WeiboPageCommon.get_all_weibo(self.browser)
         weibos = map(lambda weibo_driver:self.__get_webibo_info(weibo_driver), weibo_list)
         #WeiboPageCommon.scroll(self.browser)
@@ -25,6 +27,7 @@ class WeiboPublishCrawler:
         weibos = filter(lambda w : w is not None , weibos)
         sorted_weibos = PublishStrategy.nums_strategy(weibos)
         WeiboPageCommon.forword_weibo(self.browser, sorted_weibos[0], '')
+        self.browser.close()
         print ''
 
     def __get_webibo_info(self, weibo_driver):
