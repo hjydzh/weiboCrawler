d95b6fd8c923490290d7ad4731639a1a249f7562
hjydzh <452034476@qq.com>
2016-01-13 23:31:46 +0800

    修改爬虫登录模块

--- loginController/LoginController.py
+++ loginController/LoginController.py
@@ -18,11 +18,12 @@ def get_chrome():
 def get_firfox():
     binary = FirefoxBinary('G:/lessUsedTools/browser/firefox/firefox.exe')
     browser = webdriver.Firefox(firefox_binary=binary)
-    return browser
+    return browser.get_screenshot_as_file('1.png')
 
 
 def get_browser():
     dcap = dict(DesiredCapabilities.PHANTOMJS)
+    DesiredCapabilities.PHANTOMJS['phantomjs.page.customHeaders.Accept-Language'] = 'zh-CN,zh;q=0.8'
     phantomjs_path = Const.PHANTOMJS_PATH
     #phantomjs_path = "G:\\programeSoftwares\\python2.7\\Scripts\\phantomjs.exe"
     dcap["phantomjs.page.settings.userAgent"] = (
@@ -32,11 +33,12 @@ def get_browser():
             #"Mozilla/5.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/600.1.4 (KHTML, like Gecko) Version/8.0 Mobile/12A366 Safari/600.1.4"
             #"Mozilla/5.0 (Linux; U; Android 4.2.1; zh-CN; VOTO X2 Build/JOP40D) AppleWebKit/533.1 (KHTML, like Gecko) Version/4.0 UCBrowser/9.8.9.457 U3/0.8.0 Mobile Safari/533.1"
             #"Mozilla/5.0 (iPhone; U; ru; CPU iPhone OS 4_2_1 like Mac OS X; ru) AppleWebKit/533.17.9 (KHTML, like Gecko) Version/5.0.2 Mobile/8C148a Safari/6533.18.5"
-            "Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25"
-            #"Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"
+            #"Mozilla/6.0 (iPhone; CPU iPhone OS 8_0 like Mac OS X) AppleWebKit/536.26 (KHTML, like Gecko) Version/8.0 Mobile/10A5376e Safari/8536.25"
+            "Mozilla/5.0 (Windows NT 6.3; WOW64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/47.0.2526.106 Safari/537.36"
     )
-    browser = webdriver.PhantomJS(desired_capabilities=dcap,executable_path=phantomjs_path)
-        #self.browser = webdriver.PhantomJS(desired_capabilities=dcap)
+    browser = webdriver.PhantomJS(desired_capabilities=dcap)
+    #browser = webdriver.PhantomJS(desired_capabilities=dcap,executable_path=phantomjs_path)
+        #self.browser = webdriver.PhantomJS(desired_capabilities=dcap
     browser.set_window_size(1366, 768)
     return browser
 
@@ -68,14 +70,13 @@ def __input_login_info(browser, username, passwd):
 
 def log_in(browser, username, passwd):
     browser.get('http://weibo.com/')
-    #time.sleep(5)
+    time.sleep(5)
     browser.find_element_by_xpath("//input[@node-type='username']").send_keys(username)
     browser.find_element_by_xpath("//input[@node-type='password']").send_keys(passwd)
-    #browser.find_element_by_xpath("//a[@node-type='submitBtn']").click()
-    #time.sleep(5)
-    cookies = browser.get_cookies()
-    pickle.dump(cookies, open('cookie', 'wb'))
-    browser.get('http://weibo.com/')
+    browser.find_element_by_xpath("//a[@node-type='submitBtn']").click()
+    time.sleep(5)
+    pickle.dump(browser.get_cookies(), open('cookie', 'wb'))
+    return browser
 
 def login_by_cookie(browser):
     print '开始登录'
@@ -101,6 +102,7 @@ def login_by_cookie(browser):
     time.sleep(5)
     print '页面标题:'
     print browser.title
+    pickle.dump(browser.get_cookies(), open('cookie', 'wb'))
 
 if __name__ == '__main__':
     print ''
