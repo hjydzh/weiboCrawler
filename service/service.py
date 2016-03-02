0bf54dc51c36036c02de7964dcca4e033f74f8d7
hjydzh <452034476@qq.com>
2016-03-02 23:08:21 +0800

    change

--- service/service.py
+++ service/service.py
@@ -7,12 +7,12 @@ def send_weibo(text, url, browser):
     browser.get('http://m.weibo.cn/mblog')
     time.sleep(3)
     browser.find_element_by_id('txt-publisher').send_keys(text)
-    open(r'd:\1.jpg', 'wb').write(HttpUtil.request(url))
-    browser.find_element_by_class_name('picupload').send_keys(r'd:\1.jpg')
+    open(r'/home/1.jpg', 'wb').write(HttpUtil.request(url))
+    browser.find_element_by_class_name('picupload').send_keys(r'/home/1.jpg')
     time.sleep(4)
     browser.find_element_by_link_text('发送').click()
     time.sleep(3)
-    os.remove(r'd:\1.jpg')
+    os.remove(r'/home/1.jpg')
     print '发送成功'
 
 
