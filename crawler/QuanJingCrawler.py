4b530c1d2740cb98d2a270f78565721a9db1dee8
hjydzh <452034476@qq.com>
2016-03-05 22:48:12 +0800

    sned pics

---
+++ crawler/QuanJingCrawler.py
@@ -0,0 +1,20 @@
+#coding:utf-8
+import sys
+sys.path.append('/home/workspace/webcrawler/weiboCrawler')
+from service import service
+from loginController import LoginController
+from utils import PasswdUtil
+from utils import  DateUtil
+from  commons.TimeConst import *
+
+def search():
+    date = DateUtil.time_to_str(DateUtil.substract_day(DateUtil.now(), 1), Const.STYLE_YYMMDD)
+    txt = '视觉时刻  @当幸福敲不开门 [微笑][微笑]'
+    url = 'http://freepic.wetu.me/preview/%s%s.jpg'
+    browser = LoginController.get_browser()
+    passwd_dict = PasswdUtil.get_passwd_dict()
+    LoginController.mobile_login(browser, passwd_dict[Const.WEIBO_USERNAME], passwd_dict[Const.WEIBO_PASSWD])
+    service.send_weibo_pics(txt, [url % (date, 'i'), url % (date, 'p')], browser)
+
+if __name__ == '__main__':
+    search()
